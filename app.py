1acd1974fff8dbf5b969a7eaf64a1cf5b39e870f
Kamieljv <kamiel.verhelst@gmail.com>
2020-06-03 15:25:51 +0200

    Added templates directory with home template, added bootstrap

--- app.py
+++ app.py
@@ -14,7 +14,7 @@
 
 """
 
-from flask import Flask
+from flask import (Flask, render_template)
 
 import folium
 
@@ -24,8 +24,8 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     start_coords = (46.9540700, 142.7360300)
-    folium_map = folium.Map(location=start_coords, zoom_start=14)
-    return folium_map._repr_html_()
+    folium_map = folium.Map(location=start_coords, zoom_start=14)._repr_html_()
+    return render_template('home.html', map=folium_map)
 
 
 if __name__ == '__main__':
