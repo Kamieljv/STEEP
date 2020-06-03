c8b6b6de8df9c9ea16b7b00d898e8f85112dee8c
lihuanhuan290 <59559957+lihuanhuan290@users.noreply.github.com>
2020-06-03 22:02:46 +0200

    code from video, not finish yet

--- src/routing.py
+++ src/routing.py
@@ -21,3 +21,41 @@ try:
     pprint(api_response)
 except ApiException as e:
     print("Exception when calling MapMatchingApi->post_gpx: %s\n" % e)
+
+import requests
+import geopandas as gpd
+import osmnx as ox
+import matplotlib.pyplot as plt
+#place_name = "Berlin"
+#graph = ox.graph_from_place(place_name)
+#type(graph)
+import pandas as pd
+from pandas.io.json import json_normalize
+import json
+import csv
+url = 'https://graphhopper.com/api/1/match?vehicle=car&key=3fccaaf9-1e62-4a69-ae2c-7c4ec49ec955\" --data @/path/to/some.gpx' #osm direction api
+response = requests.get(url)
+data = response.json()
+print(data)
+df = pd.io.json.json_normalize(data)
+def flatten_json(y):
+    out = {}
+
+    def flatten(x, name=""):
+        if type(x) is dict:
+            for a in x:
+                flatten(x[a], name + a + "_")
+        elif type(x) is list:
+            i = 0
+            for a in x:
+                flatten(a, name + str(i) + "_")
+                i += 1
+        else:
+            out[name[:-1]] = x
+
+    flatten(y)
+    return out
+
+flat = flatten_json(data)
+dfObj = pd.DataFrame(flat, index=['a'])
+dfObj.to_csv(r'D:\Git\github\\test.csv')
