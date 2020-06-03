88a35ffc2094df20d7dd11af0e0c25f618b5b6cb
Alena Izotova <alena.izotova@wur.nl>
2020-06-03 21:29:36 +0200

    Add fields and button

--- app.py
+++ app.py
@@ -14,19 +14,25 @@
 
 """
 
-from flask import (Flask, render_template)
+from flask import (Flask, render_template, request)
 
 import folium
 
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route('/', methods=["GET", "POST"])
 def index():
     start_coords = (46.9540700, 142.7360300)
+
+    if request.method == 'POST':
+        start_coords = (request.form["x"], request.form["y"])
+
     folium_map = folium.Map(location=start_coords, zoom_start=14)._repr_html_()
-    return render_template('home.html', map=folium_map)
+
+    return render_template('home.html', map=folium_map, form=None)
 
 
 if __name__ == '__main__':
     app.run(debug=True)
+
