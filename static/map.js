ca35d360f5440f9d8693c8fb675d35f5b6ca1708
Alena Izotova <alena.izotova@wur.nl>
2020-06-04 17:59:28 +0200

    first attemp to make styles for road segments

--- static/map.js
+++ static/map.js
@@ -21,4 +21,42 @@ $(document).ready(function (e) {
         maxZoom: 19,
         attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
     }).addTo(map);
+    //add a route
+    var segment1 = [100, 40]
+    var segment2 = [105, 45]
+    var segment3 = [110, 55]
+    var geojsonRoute = [{
+        "type": "Feature",
+        "properties": {"speed": "low"},
+        "geometry": {
+            "type": "LineString",
+            "coordinates": [segment1, segment3]
+        }
+    }, {
+        "type": "Feature",
+        "properties": {"speed": "high"},
+        "geometry": {
+            "type": "LineString",
+            "coordinates": [segment2]
+        }
+    }];
+
+    var myStyle1 = {
+    "color": "#f17202",
+    "weight": 5,
+    "opacity": 0.65
+    };
+    var myStyle2 = {
+    "color": "#020ef1",
+    "weight": 10,
+    "opacity": 0.65
+    };
+    L.geoJSON(geojsonRoute, {
+        style: function (feature) {
+            switch (feature.properties.speed) {
+                case "low": return myStyle1;
+                case "high": return myStyle2
+            }
+        }
+    }).addTo(map);
 });
