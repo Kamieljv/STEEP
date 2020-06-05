2e110694928b4cbddbdc0026f260cd75e714aea9
Alena Izotova <alena.izotova@wur.nl>
2020-06-05 16:19:15 +0200

    add add_search and chooseAddr

--- static/map.js
+++ static/map.js
@@ -23,10 +23,39 @@ $(document).ready(function (e) {
     }).addTo(map);
 });
 
-function addr_search() {
+function add_search() {
     var inp = document.getElementById("addr");
-    $.getJSON('http://nomatin.openstreetmap.org/search?format=json&limit=5&q=' + inp.value, function(data))
-    };
+    $.getJSON('https://nominatim.openstreetmap.org/search?format=[geojson&limit=5&q=]' + inp.value, function(data) {
+        var items = [];
+        $.each(data, function(key, val) {
+            items.push(
+                "<li><a href='#' onclick='chooseAddr(" +
+                val.lat + ", " + val.lon + ");return false;'>" + val.display_name +
+                '</a></li>'
+            );
+        });
+        $('#results').empty();
+        if (items.length != 0) {
+            $('<p>', { html: "Search results:" }).appendTo('#results');
+            $('<ul/>', {
+                'class': 'my-new-list',
+                html: items.join('')
+            }).appendTo('#results');
+        } else {
+            $('<p>', { html: "No results found" }).appendTo('#results');
+        }
+  });
+}
+function chooseAddr(lat, lng, type) {
+  var location = new L.LatLng(lat, lng);
+  map.panTo(location);
+
+  if (type == 'city' || type == 'administrative') {
+    map.setZoom(11);
+  } else {
+    map.setZoom(13);
+  }
+}
 
 function addRoute(map, route) {
     /*
