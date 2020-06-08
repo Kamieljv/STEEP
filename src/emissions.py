8dbe0ede79bee132c52491d5f45c0fe8d02fce30
elikreuz <55311197+elikreuz@users.noreply.github.com>
2020-06-08 10:02:31 +0200

    Data used to calculate the emission factor
    EPAA COPERT data used to derive emission factors

---
+++ src/emissions.py
@@ -0,0 +1,5 @@
+import pandas as pd
+import numpy as np
+
+##required inputs: Car_parameters (as dictionary), segment speed(as a value)
+##outputs of emmissionfactor fuction will be a g/km value which can be used to calculate the emissions of a segment
