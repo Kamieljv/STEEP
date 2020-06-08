391f6f01c48b2be94d9886257bc94d6d7d90d6f7
elikreuz <55311197+elikreuz@users.noreply.github.com>
2020-06-08 13:48:12 +0200

    Experimented with different GeoJson routes, they are still there but I decided to discard them for the  calculations

--- src/emissions.py
+++ src/emissions.py
@@ -1,5 +1,19 @@
 import pandas as pd
 import numpy as np
+import geojson
+import os
+os.chdir('C:/Data/ACT_GIS/STEEP/')
 
 ##required inputs: Car_parameters (as dictionary), segment speed(as a value)
 ##outputs of emmissionfactor fuction will be a g/km value which can be used to calculate the emissions of a segment
+emmisions = pd.read_csv('data/EC_Factors_Passenger.csv')
+#with open("static\TestRoute_Wag_Roest.geojson") as gj_file:
+  #  gj = geojson.load(gj_file)
+#speeds = []
+#for feature in gj['features']:
+ #   print(feature['attributes'])
+
+#route =
+#speed = sample.geojson[]
+print(emmisions.head())
+
