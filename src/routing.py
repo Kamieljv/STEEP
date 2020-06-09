32af772c85c7456f0044759f43589b63939d830c
mvac <marta.arambulacoyote@wur.nl>
2020-06-09 23:02:04 +0200

    Added arrays that contain all the points of the long route and the points from the instructions

--- src/routing.py
+++ src/routing.py
@@ -68,13 +68,42 @@ def find(key, dictionary):
                         yield result
 
 # Extract points stored in legs
-points_lr = list(find('points', routing))
-long_route = points_lr[0]
+lr_points = list(find('points', routing))
+long_route = lr_points[0]
 
-# Extract points in instructions
-points_edg = list(find('point', routing))
+# Extract points from the instructions section
+seg_points = list(find('point', routing))
 
-#
+# Initialize our 2 arrays that will contain all the points of the long route
+lat_lr = []
+long_lr = []
 
+# Iterate through all the points and add the lat and long to the correct array (long route)
+for point in long_route:
+    lat_lr.append(point['latitude'])
+    long_lr.append(point['longitude'])
+
+# Initialize our 2 arrays that will contain all the points of the segmented route
+lat_seg = []
+long_seg = []
+
+# Iterate through all the points and add the lat and long to the correct array (segmented route)
+for point in seg_points:
+    lat_seg.append(point['latitude'])
+    long_seg.append(point['longitude'])
+
+## Make a geodataframe
+# Performing imports
+import pandas as pd
+import geopandas as gpd
+from shapely.geometry import Point
+
+df = pd.DataFrame(data)
+my_columns = ['lon', 'lat']
+df.columns = my_columns
+print(df.head)
+
+geometry = [Point(xy) for xy in zip(df['lon'], df['lat'])]
+routingGDF = gpd.GeoDataFrame(df, geometry=geometry)
 
 
