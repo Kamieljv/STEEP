424aa8d17e42287ee1b440fc72396361657aa347
mvac <marta.arambulacoyote@wur.nl>
2020-06-09 12:51:05 +0200

    extracting points (coordinates)

--- src/routing.py
+++ src/routing.py
@@ -36,3 +36,45 @@ params = dict(
 
 resp = requests.get(tomtomURL, params=params, headers=headers)
 data = resp.json()
+
+# Extract data
+from ast import literal_eval
+
+string = str(data)
+routing = literal_eval(string)
+
+
+def find(key, dictionary):
+    for k, v in dictionary.items():
+        if k == key:
+            yield v
+        elif isinstance(v, dict):
+            for result in find(key, v):
+                yield result
+        elif isinstance(v, list):
+            for d in v:
+                if isinstance(d, dict):
+                    for result in find(key, d):
+                        yield result
+
+
+data = list(find('points', routing))
+data = data[0]
+
+# Store as geodataframe
+import pandas as pd
+import geopandas as gpd
+from shapely.geometry import Point
+
+df = pd.DataFrame(data)
+my_columns = ['lon', 'lat']
+df.columns = my_columns
+print(df.head)
+
+geometry = [Point(xy) for xy in zip(df['lon'], df['lat'])]
+routingGDF = gpd.GeoDataFrame(df, geometry=geometry)
+routingGDF.crs = {'init': 'epsg:28992'}
+routingGDF.plot(marker='.', color='green', markersize=50)
+print(type(routingGDF), len(routingGDF))
+
+
