c3d92ec0a0ab2367338fde298d1aebedf8716724
mvac <marta.arambulacoyote@wur.nl>
2020-06-09 11:37:56 +0200

    Tomtom API. Trying to add parameters

--- src/routing.py
+++ src/routing.py
@@ -1,4 +1,4 @@
-### test
+# Tomtom Calculate route API
 import requests
 
 apiURL = 'http://maps.googleapis.com/maps/api/directions/json'
@@ -13,25 +13,28 @@ destLon = 13.41774
 # Tomtom url
 tomtomURL = "%s/%s,%s:%s,%s/json?key=%s" % (apiURL, sourceLat, sourceLon, destLat, destLon, apiKEY)
 
+# Headers
+headers = {
+    'accept': '*/*',
+}
+
 # Parameters
 params = dict(
-    versionNumber=1,
-    location='Berlin:Hamburg',
-    maxAlternatives=3,
-    alternativeType='betterRoute',
     instructionsType='text',
     language='en-GB',
+    maxAlternatives='3',
+    alternativeType='betterRoute',
+    sectionType='traffic',
     routeRepresentation='polyline',
-    computeTravelTimeFor='all',
-    sectionType='traffic'
+    report='effectiveSettings',
+    routeType='eco',
+    traffic='true',
+    avoid='unpavedRoads',
+    travelMode='car',
+    vehicleCommercial='false',
+    vehicleEngineType='combustion',
 )
 
-resp = requests.get(tomtomURL, params=params)
+resp = requests.get(tomtomURL, headers=headers, params=params)
 data = resp.json()
-
-
-
-
-
-
 
