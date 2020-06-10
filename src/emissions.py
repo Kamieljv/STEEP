74497833485cb696df815307cf88e997129a88fb
Alena Izotova <alena.izotova@wur.nl>
2020-06-10 13:34:07 +0200

    changed according to Kamiel comments but I did not fix the negative values yet

--- src/emissions.py
+++ src/emissions.py
@@ -1,42 +1,17 @@
 import pandas as pd
-import numpy as np
-# import geojson
 import os
 
-# os.chdir('C:/Data/ACT_GIS/STEEP/') # for Eli
-os.chdir('/home/alena/my_project/STEEP/')  # for Alena
-
-##required inputs: Car_parameters (as dictionary), segment speed(as a value)
-##outputs of emissionfactor fuction will be a g/km value which can be used to calculate the emissions of a segment
-# emissions = pd.read_csv('data/EC_Factors_Passenger.csv')
-# with open("static\TestRoute_Wag_Roest.geojson") as gj_file:
-#  gj = geojson.load(gj_file)
-# speeds = []
-# for feature in gj['features']:
-#   print(feature['attributes'])
-
-# route =
-# speed = sample.geojson[]
-# print(emissions.head())
-
-# final formula (Alpha * Speed ^ 2 + Beta * Speed + Gamma + (Delta/Speed) ) / (Epsilon * Speed^2 + Zita * Speed + Hta)
-
-# input will be speeds for each segment in csv
-# path is a path to the file
-# def constructSpeedDF (path):
-# spDF = read_csv(str(path))
 
 # create a sample dataFrame with speeds per segment
 
-speedPerSegmentDict = {'segment': [0, 1, 2, 3, 4],
-                       'Speed': [9, 70, 55, 80, 70]}
+#speedPerSegmentDict = {'segment': [0, 1, 2, 3, 4],
+                       #'Speed': [9, 70, 55, 80, 70]}
 
-speedPerSegmentDF = pd.DataFrame(speedPerSegmentDict)
-speedPerSegmentDF.to_csv('data/b_SpeedFlow_sample.csv')
+#speedPerSegmentDF = pd.DataFrame(speedPerSegmentDict)
+#speedPerSegmentDF.to_csv('data/b_SpeedFlow_sample.csv')
 
 # this function derive values for further emission factors calculation
 def derive_values_for_calc(path_input_emis_calc, category, fuel, segment, fuel_st, technology, pollutant):
-    # maybe we should check that all the inputs are strings before make df
     values_for_emis_calc = pd.read_csv(path_input_emis_calc)
     df_values = values_for_emis_calc[(values_for_emis_calc['Category'] == category) &
                                      (values_for_emis_calc['Fuel'] == fuel) &
@@ -44,14 +19,7 @@ def derive_values_for_calc(path_input_emis_calc, category, fuel, segment, fuel_s
                                      (values_for_emis_calc['Euro Standard'] == fuel_st) &
                                      (values_for_emis_calc['Technology'] == technology) &
                                      (values_for_emis_calc['Pollutant'] == pollutant)]
-    a = df_values['Alpha']
-    b = df_values['Beta']
-    g = df_values['Gamma']
-    d = df_values['Delta']
-    e = df_values['Epsilon']
-    z = df_values['Zita']
-    h = df_values['Hta']
-    return a, b, g, d, e, z, h
+    return df_values['Alpha'], df_values['Beta'], df_values['Gamma'], df_values['Delta'], df_values['Epsilon'], df_values['Zita'], df_values['Hta']
 
 
 # This a separate function for formula
@@ -72,21 +40,21 @@ def calculate_emission_factor(path_input_emis_calc, category, fuel, segment, fue
         speed = speed_per_seg.loc[i, 'Speed']
         if speed < 10.00:
             speed = 10.00
-            emis_f = formula_emission_factor(a, b, g, d, e, z, h, speed)
         elif speed > 130.00:
             speed = 130.00
-            emis_f = formula_emission_factor(a, b, g, d, e, z, h, speed)
         else:
             speed = speed_per_seg.loc[i, 'Speed']
-            emis_f = formula_emission_factor(a, b, g, d, e, z, h, speed)
-
+        emis_f = formula_emission_factor(a, b, g, d, e, z, h, speed)
         emis_fs += [emis_f]
     emis_per_seg = pd.DataFrame(emis_fs)
-    emis_per_seg.to_csv('data/c02_emisFactor_perSeg_perCar.csv')
+    if os.path.exists('../data/c02_emisFactor_perSeg_perCar.csv'):
+        return emis_per_seg
+    else:
+        emis_per_seg.to_csv('../data/c02_emisFactor_perSeg_perCar.csv')
     return emis_per_seg
 
 
 # input to check the result
-result = calculate_emission_factor('data/Ps_STEEP_a_emis.csv', 'Passenger Cars', 'Petrol', 'Mini', 'Euro 4', 'GDI',
-                                   'EC', 'data/b_SpeedFlow_sample.csv')
+result = calculate_emission_factor('../data/Ps_STEEP_a_emis.csv', 'Passenger Cars', 'Petrol', 'Mini', 'Euro 4', 'GDI',
+                                   'EC', '../data/b_SpeedFlow_sample.csv')
 print(result)
