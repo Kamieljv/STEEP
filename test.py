bfc71c3c22a51aff36bba91514f718401056a7f7
mvac <marta.arambulacoyote@wur.nl>
2020-06-24 09:38:44 +0200

    Update test.py
    random tests

--- test.py
+++ test.py
@@ -1,28 +1,35 @@
 # TEST time window
-from datetime import datetime
-from datetime import timedelta
+from datetime import datetime, timedelta
 
-dep = '2020-06-19 12:00'
+dep = '2020-06-23 20:10'
 departure = datetime.strptime(dep, "%Y-%m-%d %H:%M")
 initial = departure
 
-departures = [initial]
+departures = []
+if departure-timedelta(minutes=11) >= datetime.now():
+    departure -= timedelta(minutes=10)
+
+departures.append(initial)
 for i in range(4):
-    departure += timedelta(minutes=5)
+    departure += timedelta(minutes=(5*i))
     departures.append(departure)
 
-#make a emissions list
-em_list = [32.32, 46.54, 34.38, 67, 54]
-def diff_emission(list):
-    diff_list = []
-    for i in range(len(list)):
-        if i <= len(list)-2:
-            diff = round((list[i+1] - list[0]),2)
-            i = i + 1
-            diff_list.append(diff)
-    return diff_list
+########################################################################3
+def calculate_route():
+    """Gets route configuration; calculates route; returns route to view as JSON."""
+    # Check if time is not in past, otherwise change to present
+    departure = datetime.strptime(request.form['departure'], '%Y-%m-%d %H:%M')
+    if departure < datetime.now():
+        departure = datetime.now() + timedelta(minutes=1)
 
+    initial = departure
+    # Adding 4 more departure times
+    departures = []
+    if departure - timedelta(minutes=11) >= datetime.now():
+        departure -= timedelta(minutes=10)
 
-
-
-
+    departures.append(initial)
+    # Create time window
+    for i in range(4):
+        departure += timedelta(minutes=(5*1))
+        departures.append(datetime.strftime(departure, '%Y-%m-%d %H:%M'))
