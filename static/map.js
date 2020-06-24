5af6d07dcf00817f0f457ceef48418f2c1b6b7f1
lihuanhuan290 <59559957+lihuanhuan290@users.noreply.github.com>
2020-06-24 19:14:07 +0200

    change the color sides around

--- static/map.js
+++ static/map.js
@@ -204,10 +204,10 @@ function showTimewindow(response) {
         if (i !== 2) {
             let width = Number(item.emissions - defaultEmissions).toFixed(2)
             width = Number(width);
-            if (width > 0) {
-                item.leftWidth = width;
+            if (width < 0) {
+                item.leftWidth = width * -1;
             } else {
-                item.rightWidth = width * -1;
+                item.rightWidth = width;
             }
         }
         let resultHtml = getItemHtml(item);
@@ -233,7 +233,7 @@ function getItemHtml(itemInfo) {
          +      `<div class="img_center"></div>`
          +      `<div class="img_right" style="width: ${itemInfo.rightWidth ? itemInfo.rightWidth : 0}px"></div>`
          +  '</div>'
-         +  `<div class="bottom_text">${itemInfo.leftWidth ? '+' : itemInfo.rightWidth ? '-' : ''}${itemInfo.leftWidth || itemInfo.rightWidth || em}</div>`
+         +  `<div class="bottom_text">${itemInfo.leftWidth ? '-' : itemInfo.rightWidth ? '+' : ''}${itemInfo.leftWidth || itemInfo.rightWidth || em}</div>`
          + '</div>'
          +'</div>';
     return resultHtml;
