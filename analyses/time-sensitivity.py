54be37d03929930cbee4cfdf4c7d52403599d257
elikreuz <55311197+elikreuz@users.noreply.github.com>
2020-06-29 15:09:03 +0200

    changes to plotting

--- analyses/time-sensitivity.py
+++ analyses/time-sensitivity.py
@@ -44,7 +44,6 @@ def timeProfileLoop(date, timestep=10, days=1):
 
     # Start time loop
     while departure <= maxdate:
-        departure += timedelta(minutes=timestep)
 
         dep_string = tz.localize(departure).strftime(fmt)
         dep_string = dep_string[:-2] + ':' + dep_string[-2:]
@@ -62,26 +61,46 @@ def timeProfileLoop(date, timestep=10, days=1):
         stats = calculator.calculate_stats()
         df_row = pd.DataFrame([[dep_string] + list(stats) + [routetype, fuel, segment, standard, startLat, startLon, destLat, destLon]], columns=cols)
         df_timeprofile = df_timeprofile.append(df_row)
+        departure += timedelta(minutes=timestep)
 
-        df_timeprofile.to_csv('output/timeprofile_'+ datetime.strftime(datetime.now(), '%Y%m%dT%H%M') +'.csv', index=False)
+    df_timeprofile.to_csv('output/timeprofile_'+ datetime.strftime(datetime.now(), '%Y%m%dT%H%M') +'.csv', index=False)
 
-def plotTimeProfile(file):
+def plotTimeProfile(file, file2, file3):
 
     # Read from csv file into pandas dataframe
     df_timeprofile = pd.read_csv(file, index_col=0, parse_dates=True)
-
+    df_timeprofile['time'] = df_timeprofile['time']/60
+    df_timeprofile['distance'] = df_timeprofile['distance']/1000
+    df_timeprofile2 = pd.read_csv(file2, index_col=0, parse_dates=True)
+    df_timeprofile2['time'] = df_timeprofile2['time'] / 60
+    df_timeprofile2['distance'] = df_timeprofile2['distance'] / 1000
+    df_timeprofile3 = pd.read_csv(file3, index_col=0, parse_dates=True)
+    df_timeprofile3['time'] = df_timeprofile3['time'] / 60
+    df_timeprofile3['distance'] = df_timeprofile3['distance'] / 1000
     fig, (ax1, ax2, ax3) = plt.subplots(1, 3)
 
-    df_timeprofile['time'].plot(ax=ax1)
+    df_timeprofile['time'].plot(ax=ax1, label="10 AM")
+    df_timeprofile2['time'].plot(ax=ax1, color='r', label='5 PM')
+    df_timeprofile3['time'].plot(ax=ax1, color='g', label='6 PM')
     ax1.set_title('Time')
-    df_timeprofile['emissions'].plot(ax=ax2)
-    ax2.set_title('Emissions')
-    df_timeprofile['distance'].plot(ax=ax3)
-    ax3.set_title('Distance')
+    ax1.set(xlabel="Date of Departure", ylabel="Travel Time (min)")
 
+    df_timeprofile['emissions'].plot(ax=ax2, label="10 AM")
+    df_timeprofile2['emissions'].plot(ax=ax2, color='r', label='5 PM')
+    df_timeprofile3['emissions'].plot(ax=ax2, color='g', label='6 PM')
+    ax2.set_title('Emissions')
+    ax2.set(xlabel="Date of Departure", ylabel="CO2 Emissions (kg)")
+
+    df_timeprofile['distance'].plot(ax=ax3, label="10 AM")
+    df_timeprofile2['distance'].plot(ax=ax3, color='r', label='5 PM')
+    df_timeprofile3['distance'].plot(ax=ax3, color='g', label='6 PM')
+    ax3.set_title('Distance')
+    ax3.set(xlabel="Date of Departure", ylabel="Travel Distance (km)")
+
+    plt.legend(loc='best')
     plt.show()
 
 
 if __name__ == '__main__':
-   # timeProfileLoop("2020-07-01 00:00", timestep=60, days=1)
-    plotTimeProfile('output/timeprofile_20200625T1524.csv')
+   #timeProfileLoop("2020-06-28 17:00", timestep=1440, days=7)
+   plotTimeProfile('output/timeprofile_20200625T1648.csv', 'output/timeprofile_20200628t0927.csv', 'output/timeprofile_20200625T1642.csv')
